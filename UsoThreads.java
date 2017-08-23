748955647282aff0d44a620bb0dfeee02403c588
Espadaclar <eneroborde8002@gmail.com>
2017-08-23 04:50:15 +0200

    Aplicacion de Threads video 168.

--- UsoThreads.java
+++ UsoThreads.java
@@ -8,8 +8,14 @@ import java.awt.event.*;
 /**
  * THREADS,  programación concurrente, pueden realizar varias tareas simultaneamente. 
  * Hasta ahora hemos programado monotareas, esto es. SOLO SE EJECUTABA UN THREAD,  UN HILO.
-
-
+ * 
+ *  *  ======================================== PASOS PARA PODER EJECUTAR VARIOS HILOS =====================
+ * 1º Crear cl que implemente interface Runnable. 
+ * 2º Escribir el codigo de la tarea dentro del mt run():
+ * 3º Instanciar la cl creada y almacenar la instancia en variable de tipo Runnable.
+ * 4º Crear instancia de la cl Thread pasando como parametro al constructor de Thread el objeto Runnable anterior.
+ * 5º Poner en marcha el hilo de ejecución con el mt start() de la cl Thread.
+ *
  * @author franciscoJavier
  */
 public class UsoThreads {
@@ -125,12 +131,21 @@ class MarcoRebote extends JFrame{
         c.add(boton);
         boton.addActionListener(oyente);
     }
-    //Añade pelota y la bota 1000 veces
+    
+    /**
+     * 1º instancia la cl Pelota y crea una pelota.
+     * 2º agrega a la lamina la pelota.
+     * 3º pasa la cl 'Hilo' que implementa la interface Runnable la pelota y la lamina y almacena todo esto en una VL de tipo Runnable.
+     * 4º crea un tarea con ese Runnable
+     * 5º le dice que comience la tarea
+     * LA VENTAJA DE ESTO ES QUE CADA VEZ QUE INVOQUEMOS ESTE MT DESDE EL BOTÓN VA A CREAR UNA PELOTA EN LA LAMINA.
+     */
     public void comienza_el_juego (){
         Pelota pelota=new Pelota();
         lamina.add(pelota);
-        //bucle que llama 3000 veces al mt, mueve_pelota, el cual mueve la pelota una posición cada vez.
-        for (int i=1; i<=3000; i++){
+        
+        // ------ PARA EJECUTAT VARIOS THEADS PASAMOS ESTE CÓDIGO A LA CL, CON LA INTERFARCE
+        /* for (int i=1; i<=3000; i++){
             pelota.mueve_pelota(lamina.getBounds());
             lamina.paint(lamina.getGraphics());
             //para hacer una pausa en la ejecucion de un hilo. Todos los programas que hemos hecho hasta ahora
@@ -138,7 +153,54 @@ class MarcoRebote extends JFrame{
             try{
                 Thread.sleep(4);
             }catch(InterruptedException e){
+            }
+        }*/
+        
+       /* *  *  ======================================== PASOS PARA PODER EJECUTAR VARIOS HILOS =====================
+ 
+        * 3º Instanciar la cl creada y almacenar la instancia en variable de tipo Runnable.
+        * 4º Crear instancia de la cl Thread pasando como parametro al constructor de Thread el objeto Runnable anterior.
+        * 5º Poner en marcha el hilo de ejecución con el mt start() de la cl Thread.*/
 
+        Runnable r = new Hilo(pelota, lamina);
+        Thread t = new Thread(r);
+        t.start();
+    }
+    
+    /**
+     * *  ======================================== PASOS PARA PODER EJECUTAR VARIOS HILOS =====================
+     * 1º Crear cl que implemente interface Runnable. 
+     */
+    private class Hilo implements Runnable {
+
+        private Pelota pelota;
+        private Component componente;
+
+        public Hilo(Pelota pelota, Component componente) {
+            this.pelota = pelota;
+            this.componente = componente;
+        }
+
+        /**
+         *  ======================================== PASOS PARA PODER EJECUTAR VARIOS HILOS =====================
+        * 2º Escribir el codigo de la tarea dentro del mt run():
+         */
+        @Override
+        public void run() {
+            //bucle que llama 3000 veces al mt, mueve_pelota, el cual mueve la pelota una posición cada vez.
+            for (int i = 1; i <= 3000; i++) {
+                pelota.mueve_pelota(componente.getBounds());
+                componente.paint(componente.getGraphics());
+                if (i == 3000) {
+
+                }
+                //para hacer una pausa en la ejecucion de un hilo. Todos los programas que hemos hecho hasta ahora
+                // solo tienen un hilo. este tendrá varios hilos
+                try {
+                    Thread.sleep(0, 4);
+                } catch (InterruptedException e) {
+                    System.out.println("Herro. ¡¡¡¡");
+                }
             }
         }
     }

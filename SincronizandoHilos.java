5026cfce6b25bc8005ecb4705d2f927c1fc3bf15
Espadaclar <eneroborde8002@gmail.com>
2017-08-24 09:18:05 +0200

    Sincronizados_II varios Threads con mt, 'join()' video 171.

--- SincronizandoHilos.java
+++ SincronizandoHilos.java
@@ -8,19 +8,21 @@ import java.util.logging.Logger;
  * (el orden de mostrar los hilos en pantalla depende del SO y de los recursos que esté utilizando el 
  * ordenador en ese momento.)
  * 
- * HABRA OCOASIONES EN QUE TENGAMOS QUE UTILIZAR UN HILO ANTES QUE OTRO, PARA ELLO SE 
+ * HABRA OCOASIONES EN QUE TENGAMOS QUE UTILIZAR UN HILO ANTES QUE se efecute OTRO, PARA ELLO SE 
  * NECESITA -------- SINCRONIZAR LOS HILOS. -------
  * 
+ * -------------------- utilizamos el mt 'join()'
  * @author Usuario
  */
 public class SincronizandoHilos {
     public static void main(String[] args) {
         HilosVarios hilo1 = new HilosVarios();
         HilosVarios hilo2 = new HilosVarios();
-        HilosVarios hilo3 = new HilosVarios();
         hilo1.start();
+        // --APLICANDO EL MT, 'join()' A CADA UNO DE LOS HILOS CONSEGUIMOS QUE SE SINCRONICEN, 
+        // --HASTA QUE NO QUEDE EN ESTADO DE MUERTE EL 1º HILO CON EL MT, NO PASA A EJECUTARSE EL SIGUIENTE.
         try {
-            hilo1.join();
+            hilo1.join();// join() ejecuta el hilo hasta que quede en estado de muerte.
         } catch (InterruptedException ex) {
             Logger.getLogger(SincronizandoHilos.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -30,11 +32,15 @@ public class SincronizandoHilos {
         } catch (InterruptedException ex) {
             Logger.getLogger(SincronizandoHilos.class.getName()).log(Level.SEVERE, null, ex);
         }
-        hilo3.start();
+        System.out.println("Hola, qué hay, cómo estamos??");
     }  
 }
 
 class HilosVarios extends Thread {
+    /**
+     * mt que ejecuta el código que tiene dentro de su cuerpo.
+     * para que lo ejecute de forma lenta, que pueda apreciarlo el ojoHumano, le aplicamos el mt sleep()
+     */
     public void run() {
         for (int i = 0; i < 5; i++) {
             System.out.println("Ejecutando hilo " +getName());
